b3ec04da76ff9b525fe00080bc37edb65839f7d5
feriwnarta <feriwnarta26@gmail.com>
2021-09-09 21:18:21 +0700

    starter projek

---
+++ src/main/java/com/ferdev/jdbcfull/model/Produk.java
@@ -0,0 +1,14 @@
+package com.ferdev.jdbcfull.model;
+
+public class Produk {
+    private String id;
+    private String nama;
+    private String deskripsi;
+    private String harga;
+    private String quantity;
+    private String waktuDibuat;
+    private String sample;
+    private String contoh;
+
+
+}
